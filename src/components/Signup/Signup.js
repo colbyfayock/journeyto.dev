0fa7fb1564c62244024adea92233ee13198e45a7
colbyfayock <fayock@gmail.com>
2021-02-07 11:04:58 -0500

    tracking button clicks

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+
+import { event } from 'lib/gtag';
 import ClassName from 'models/classname';
 
 import Form from 'components/Form';
@@ -8,12 +11,33 @@ import Button from 'components/Button';
 import styles from './Signup.module.scss';
 
 const Signup = ({ children, className, ...rest }) => {
+  const [hasSubmitted, setHasSubmitted] = useState(false);
   const signupClassName = new ClassName(styles.signup);
 
   signupClassName.addIf(className, className);
 
+  function handleOnSubmit(e) {
+    e.preventDefault();
+    e.persist();
+
+    setHasSubmitted(true);
+
+    const { currentTarget } = e;
+
+    const test = event({
+      category: 'form',
+      action: 'signup',
+      label: 'Start Your Dev Journey'
+    });
+
+    setTimeout(() => {
+      currentTarget.submit();
+      setHasSubmitted(false);
+    }, 1000);
+  }
+
   return (
-    <Form className={styles.signup} action="https://app.convertkit.com/forms/1840044/subscriptions" method="post">
+    <Form className={styles.signup} action="https://app.convertkit.com/forms/1840044/subscriptions" method="post" onSubmit={handleOnSubmit}>
       <FormRow>
         <p>
           Enter your email and get this <strong>FREE</strong> newsletter right to your inbox!
@@ -23,7 +47,7 @@ const Signup = ({ children, className, ...rest }) => {
         <Input type="email" name="email_address" aria-label="Enter your email address" placeholder="Email Address" required />
       </FormRow>
       <FormRow>
-        <Button>Start Your Dev Journey</Button>
+        <Button disabled={hasSubmitted}>Start Your Dev Journey</Button>
       </FormRow>
     </Form>
   )
