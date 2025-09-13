1aa45a572ef3ebde5b91d11ba14074157cdb47ea
Colby Fayock <fayock@gmail.com>
2025-09-13 11:04:51 -0400

    config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,5 @@
-const siteConfig = {
-  siteName: 'My Product',
-  gaPropertyId: 'XX-123456789-1'
-};
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    ...siteConfig,
-    NEXT_PUBLIC_siteName: 'My Product'
-  },
-  images: {
-    unoptimized: true
-  }
 };
 
 module.exports = nextConfig;
