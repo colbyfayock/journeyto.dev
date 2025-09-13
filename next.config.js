d9df28f0c5ed9a80eeb076bb43d5b0c5c7516f0f
Colby Fayock <fayock@gmail.com>
2025-09-12 23:16:27 -0400

    updates

--- next.config.js
+++ next.config.js
@@ -1,16 +1,17 @@
-const withPlugins = require('next-compose-plugins');
-const images = require('next-images');
-
 const siteConfig = {
   siteName: 'My Product',
   gaPropertyId: 'XX-123456789-1'
 };
 
-module.exports = withPlugins([
-  images
-], {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   env: {
     ...siteConfig,
     NEXT_PUBLIC_siteName: 'My Product'
+  },
+  images: {
+    unoptimized: true
   }
-});
+};
+
+module.exports = nextConfig;
